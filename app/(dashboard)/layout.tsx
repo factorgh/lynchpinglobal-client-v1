ce9780f70ef6c18710438b06630f735a6321b7fb
factorgh <burchellsbale@gmail.com>
2024-11-24 13:10:47 +0000

    update pushed

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,12 +1,10 @@
-import { AuthProvider } from "@/context/authContext";
 import DashboardWrapper from "../(components)/dashboardWrapper";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <DashboardWrapper>
-      {" "}
-      <AuthProvider>{children}</AuthProvider>
-    </DashboardWrapper>
+    <div>
+      <DashboardWrapper>{children}</DashboardWrapper>
+    </div>
   );
 };
 
